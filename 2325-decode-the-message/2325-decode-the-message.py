4ea2f916b3e12c59bd541fc524dca4c7a4967cbe
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2026-03-03 00:13:37 +0530

    Time: 4 ms (19.04%), Space: 19.1 MB (89.53%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -0,0 +1,16 @@
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        idx = 0
+        mp={}
+        for i in key:
+            if i != " " and i not in mp:
+                mp[i] = alphabet[idx]
+                idx += 1
+        ans = ""
+        for i in message:
+            if i != " ":
+                ans += mp[i]
+            else:
+                ans += " "
+        return ans

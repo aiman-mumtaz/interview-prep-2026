8298ddc7b1facd42a9c668d68221304a1b8446af
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2026-03-06 23:55:49 +0530

    Time: 5 ms (25.18%), Space: 20.2 MB (19.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -2,14 +2,10 @@ class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         rows, cols = len(matrix), len(matrix[0])
         first_col_has_zero = False
-
-        # 1. Iterate through the matrix to mark zeros
         for r in range(rows):
-            # Check if the first column needs to be zeroed later
             if matrix[r][0] == 0:
                 first_col_has_zero = True
             
-            # Use the first row and first col as markers (starting from col 1)
             for c in range(1, cols):
                 if matrix[r][c] == 0:
                     matrix[0][c] = 0

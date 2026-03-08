14c6dfc9073238e97e56d7e7d7e6b1496a1e4e6a
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2026-03-08 23:11:20 +0530

    Time: 6 ms (20.26%), Space: 19.3 MB (71.17%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -6,17 +6,10 @@ class Solution:
         counts = Counter()
         
         for num in nums:
-            # How many times have we seen this number before?
             row_idx = counts[num]
-            
-            # If the current frequency requires a new row, add one
             if row_idx >= len(res):
                 res.append([])
-            
-            # Place the number in the corresponding row
             res[row_idx].append(num)
-            
-            # Increment frequency for next time we see this number
             counts[num] += 1
             
         return res
